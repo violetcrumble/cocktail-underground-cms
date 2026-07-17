399b4d60a12be8f3eedd00664fad15128ac79d76
Bonnie <bonniemellott@gmail.com>
2026-07-16 20:20:31 -0400

    Initial Strapi 5 project setup
    Scaffolded clean Strapi 5 project with the 5 content types ported from the
    Strapi 4 project (recipe, spirit, blog-post, blog-author, product), the
    GraphQL plugin with v4CompatibilityMode enabled, and the Cloudinary upload
    provider configured to match the existing setup.

---
+++ config/plugins.js
@@ -0,0 +1,56 @@
+const allowedMediaTypes = [
+  'image/*',
+  'video/*',
+  'audio/*',
+  'application/pdf',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.*',
+  'text/plain',
+  'text/csv',
+];
+
+const deniedExecutableTypes = [
+  'application/vnd.microsoft.portable-executable',
+  'application/x-msdownload',
+  'application/x-msdos-program',
+  'application/x-executable',
+  'application/x-dosexec',
+  'application/x-sh',
+  'text/x-shellscript',
+  'application/x-mach-binary',
+];
+
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtManagement: 'refresh',
+      sessions: {
+        httpOnly: true,
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      security: {
+        allowedTypes: allowedMediaTypes,
+        deniedTypes: deniedExecutableTypes,
+      },
+    },
+  },
+  graphql: {
+    config: {
+      v4CompatibilityMode: env.bool('STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE', true),
+    },
+  },
+});
